4833bd5cf1ec2e0ee2201fc3807098ac6a14fbc5
abdout <osmanabdout@hotmail.com>
2024-02-26 14:54:18 +0300

    dd

--- src/model/connect/db.ts
+++ src/model/connect/db.ts
@@ -12,6 +12,18 @@ const connectDB = async (): Promise<void> => {
   } catch (error) {
     console.error('Error connecting to MongoDB:', (error as Error).message);
   }
+
+  mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to db');
+  });
+
+  mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error:', err.message);
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+  });
 };
 
 export default connectDB;
