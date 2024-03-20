9e475f0ad624ef837806c92b137d59e24f1031c7
Databayt <osmanabdout@hotmail.com>
2024-03-20 15:03:27 +0300

    5

---
+++ src/app/(auth)/password/page.tsx
@@ -0,0 +1,9 @@
+import { NewPasswordForm } from "@/component/auth/new-password-form";
+
+const NewPasswordPage = () => {
+  return ( 
+    <NewPasswordForm />
+   );
+}
+ 
+export default NewPasswordPage;
