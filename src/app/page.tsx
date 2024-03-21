91a59008059cccb1dc6578d1eb33d33c9c7cbaa9
Databayt <osmanabdout@hotmail.com>
2024-03-21 16:58:44 +0300

    5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import MdButton from "@/component/atom/button/md";
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="space-y-4 text-center">
-        <h1>Databayt</h1>
-        <p>Business Automation</p>
+      <div className="space-y-2 text-center">
+        <h1 className="text-3xl">Databayt</h1>
+        <p className="text-2xl">Business Automation</p>
         <div className="pt-6">
           <LoginButton asChild>
             <MdButton placeholder="Get started" />
