3b0dfd26c30c204e6dc7273c0c03f52490f5249d
Databayt <osmanabdout@hotmail.com>
2024-03-21 16:17:11 +0300

    8

--- src/component/auth/header.tsx
+++ src/component/auth/header.tsx
@@ -17,10 +17,10 @@ export const Header = ({
   return (
     <div className="w-full flex flex-col gap-y-4 items-center justify-center">
       <h1 className={cn(
-        "text-3xl font-semibold",
+        "text-2xl tracking-wider font-medium",
         font.className,
       )}>
-        🔐 Auth
+        Databayt
       </h1>
       <p className="text-muted-foreground text-sm">
         {label}
