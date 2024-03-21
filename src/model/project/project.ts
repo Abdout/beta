d3d233421fa5faeb63bbbb60f5e6da9c319c21b1
Databayt <osmanabdout@hotmail.com>
2024-03-21 15:20:13 +0300

    o

---
+++ src/model/project/project.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+
+const optionSchema = new Schema({
+  label: String,
+  value: String,
+});
+
+const voltageOptionSchema = new Schema({
+  lvSwgr: [optionSchema],
+  lvTrafo: [optionSchema],
+  lvCable: [optionSchema],
+  lvRmu: [optionSchema],
+  mvSwgr: [optionSchema],
+  mvTrafo: [optionSchema],
+  mvCable: [optionSchema],
+  mvRmu: [optionSchema],
+  hvSwgr: [optionSchema],
+  hvTrafo: [optionSchema],
+  hvCable: [optionSchema],
+  hvRmu: [optionSchema],
+  evSwgr: [optionSchema],
+  evTrafo: [optionSchema],
+  evCable: [optionSchema],
+  evRmu: [optionSchema],
+});
+
+const projectSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    voltages: { LV: Boolean, MV: Boolean, HV: Boolean, EV: Boolean },
+    lvOptions: voltageOptionSchema,
+    mvOptions: voltageOptionSchema,
+    hvOptions: voltageOptionSchema,
+    evOptions: voltageOptionSchema,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Project = mongoose.models.Project || mongoose.model("Project", projectSchema);
+
+export default Project;
